fe0b68729e6899697d58ca81f7862beb605b9fcf
ADITYA PRASANN <aditya.prasann@gmail.com>
2018-01-14 20:42:01 +0800

    added more fun assign

--- assignmentudemy/methodassess.py
+++ assignmentudemy/methodassess.py
@@ -70,3 +70,29 @@ def ispangram(st):
     return cmpst == string.ascii_lowercase
 
 print(ispangram('The quick brown fox jumps over the lazy dog'))
+
+print(bin(1024))
+print(hex(1024))
+print(round(5.23222,2))
+
+s = 'hello how are you Mary, are you feeling okay?'
+if(s.lower() == s ):
+    print(" is lower ")
+else:
+    print(" is not lower ")
+
+s = 'twywywtwywbwhsjhwuwshshwuwwwjdjdid'
+print(s.count('w'))
+
+set1 = {2,3,1,5,6,8}
+set2 = {3,1,7,5,6,8}
+print(set1.difference(set2))
+print(set1.union(set2))
+
+print({x: x ** 3 for x in range(5)})
+
+l = [1,2,3,4]
+print(l.reverse())
+
+l = [3,4,2,5,1]
+print(l.sort())
