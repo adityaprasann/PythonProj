b63464772c654131530a0c55736e42cad309d079
ADITYA PRASANN <aditya.prasann@gmail.com>
2018-01-21 15:26:48 +0800

    largest contiguous sum

---
+++ dsfunnexercise/largestcontiguoussum.py
@@ -0,0 +1,30 @@
+# Given an array of integers (positive and negative) find the largest continuous sum.
+
+def large_cont_sum(arr):
+    sum = 0
+    idx = 0
+    tempsum = 0
+    while(idx < len(arr)):
+        for num in arr[idx:len(arr)]:
+            tempsum = tempsum + num
+            sum = max(sum, tempsum)
+        idx += 1
+        tempsum = 0
+    return sum
+
+def large_cont_sum2(arr):
+    if len(arr)==0:
+        return 0
+    max_sum = current_sum = arr[0]
+    for num in arr[1:]:
+        current_sum = max(current_sum+num, num)
+        max_sum = max(current_sum, max_sum)
+    return max_sum
+
+print(large_cont_sum([1,2,-1,3,4,10,10,-10,-1]))
+print(large_cont_sum([1,2,-1,3,4,-1]))
+print(large_cont_sum([-1,1]))
+
+print(large_cont_sum2([1,2,-1,3,4,10,10,-10,-1]))
+print(large_cont_sum2([1,2,-1,3,4,-1]))
+print(large_cont_sum2([-1,1]))
